c7314afc7bac13d742f7efed200f67a55be6c1cf
Hyeonji-Ahn <66628832+Hyeonji-Ahn@users.noreply.github.com>
2023-10-10 13:33:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mycalendartets/MycalendarActivity.java
@@ -0,0 +1,14 @@
+package com.example.mycalendartets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MycalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mycalendar);
+    }
+}
